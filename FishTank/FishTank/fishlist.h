7eef96b1232e47320c5683af8eabce637351d271
HellWiz <32746592+HellWiz@users.noreply.github.com>
2017-10-12 22:38:52 +0300

    Add files via upload

---
+++ FishTank/FishTank/fishlist.h
@@ -0,0 +1,19 @@
+#pragma once
+class FISHLIST{
+public:
+	FISHLIST *Next;
+	int FishNum;
+	int x;
+	int y;
+	int xMin;
+	int xMax;
+	int xInc;
+	int yMin;
+	int yMax;
+	int yInc;
+	int Frame;
+	int xDir;
+	int yDir;
+	int FishDir;
+	int FrameCounter;
+};
